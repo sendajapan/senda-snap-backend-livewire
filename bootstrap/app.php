63d28b89ca861223228fe8d26be82c377b3c72b1
MD SULAIMAN <smensulaiman007@gmail.com>
2026-01-22 18:14:59 +0900

    build: Add Webhook Route Fot Github Brushup Final

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'deploy/github',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // Handle authentication exceptions for API
